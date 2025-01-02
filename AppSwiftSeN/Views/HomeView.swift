430e2996abd75c54e436b84ff231ad7581d3ca55
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-02 15:43:33 -0500

    Añadido archivo .gitignore para ignorar el modelo con la clave api

---
+++ AppSwiftSeN/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  AppSwiftSeN
+//
+//  Created by German David Vertel Narvaez on 2/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
