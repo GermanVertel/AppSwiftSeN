ed55d8f991b6a5eab93ca160edda970ecd1a7669
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-30 17:14:35 -0500

    se crea el archivo APIResponse que  maneja la estructura de los datos que OpenAI devuelve al generar imágenes. Esta información incluye detalles sobre las imagenes generadas, como URLs, descriptores y más.

--- AppSwiftSeN/Models/APIResponse.swift
+++ AppSwiftSeN/Models/APIResponse.swift
@@ -5,3 +5,13 @@
 //  Created by German David Vertel Narvaez on 29/01/25.
 //
 
+import Foundation
+
+struct APIResponse: Decodable {
+    struct ImageResponse: Decodable {
+        let url: URL
+    }
+
+    let created: Int
+    let data: [ImageResponse]
+}
