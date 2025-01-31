8ad414aec3b5eea86a9cc3ae6f6a851ce270dc30
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-30 20:11:21 -0500

    se actualizo la vista de favoritos parra que sea compatible con swiftData

--- AppSwiftSeN/Views/FavoritosView.swift
+++ AppSwiftSeN/Views/FavoritosView.swift
@@ -6,25 +6,56 @@
 //
 
 import SwiftUI
-
-
+import SwiftData
 
 struct FavoritosView: View {
-    @Binding var favoritos: [IdentifiableImage] // Array de imágenes favoritas
+    @Query(filter: #Predicate<ImageModel> { $0.isFavorite }) private var favoritos: [ImageModel]
+    @Environment(\.modelContext) private var modelContext // Contexto de SwiftData
 
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(favoritos) { imagen in
-                    Image(uiImage: imagen.image)
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(13)
-                        .padding()
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    if favoritos.isEmpty {
+                        Text("No hay imágenes favoritas")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                            .padding()
+                    } else {
+                        ForEach(favoritos) { imagen in
+                            VStack {
+                                Image(uiImage: imagen.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .cornerRadius(13)
+                                    .padding()
+
+                                Text("ID: \(imagen.id)")
+                                Text("Fecha: \(imagen.createdAt)")
+                                
+                                Button(action: {
+                                    marcarComoNoFavorito(imagen: imagen)
+                                }) {
+                                    HStack {
+                                        Image(systemName: "heart.fill")
+                                        Text("Quitar de Favoritos")
+                                    }
+                                    .foregroundColor(.red)
+                                }
+                                .padding(.bottom, 10)
+                            }
+                        }
+                    }
                 }
             }
+            .navigationTitle("Favoritos")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle("Favoritos")
-        .navigationBarTitleDisplayMode(.inline)
+    }
+
+    // Función para quitar una imagen de favoritos
+    func marcarComoNoFavorito(imagen: ImageModel) {
+        imagen.isFavorite = false
+        try? modelContext.save() // Guardar cambios en SwiftData
     }
 }
