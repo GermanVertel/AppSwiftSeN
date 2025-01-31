b10c962a354f9e1063b029604c9b9b7a3f6c27ab
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-30 20:20:23 -0500

    se configura el contenedor de swiftData

--- AppSwiftSeN/AppSwiftSeNApp.swift
+++ AppSwiftSeN/AppSwiftSeNApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct AppSwiftSeNApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: ImageModel.self) // Configura el contenedor de SwiftData
         }
     }
 }
