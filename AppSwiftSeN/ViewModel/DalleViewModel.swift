8318fce9956be4b6ec9e6ef3a025a6137d6cdced
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-30 20:22:33 -0500

    se actualiza el ViewModel ajusto a swiftData

--- AppSwiftSeN/ViewModel/DalleViewModel.swift
+++ AppSwiftSeN/ViewModel/DalleViewModel.swift
@@ -9,43 +9,53 @@
 import Foundation
 
 class DalleViewModel: ObservableObject {
-    
-    let session = UUID().uuidString
-    @Published var favoritos: [IdentifiableImage] = []
+    @Published var favoritos: [ImageModel] = []
+    let session: String
 
-    
-    func generarImagen(texto:String) async throws -> ImageModel {
-        
-        
-        guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {  throw ImageError.badURL }
-        
+    init() {
+        self.session = UUID().uuidString
+    }
+
+    func generarImagen(texto: String) async throws -> ImageModel {
+        guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {
+            throw ImageError.badURL
+        }
+
         let parametros: [String: Any] = [
             "prompt": texto,
-            "n":1,
-            "size":"1024x1024",
-            "user":session
+            "n": 1,
+            "size": "1024x1024",
+            "user": self.session
         ]
-        
+
         let data: Data = try JSONSerialization.data(withJSONObject: parametros)
-        
-        // en apiKey debes colocar tu clave de OpenAI
-        
         let apikey = ""
         var request = URLRequest(url: url)
-        
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
         request.httpBody = data
-        
+
         let (response, _) = try await URLSession.shared.data(for: request)
-        let result = try JSONDecoder().decode(ImageModel.self, from: response)
-        return result
         
+        // Decodificar la respuesta de la API
+        let apiResponse = try JSONDecoder().decode(APIResponse.self, from: response)
+        
+        // Convertir la respuesta de la API en una instancia de ImageModel
+        guard let imageURL = apiResponse.data.first?.url else {
+            throw ImageError.imageCreationFailed
+        }
+        
+        // Descargar la imagen desde la URL
+        let (imageData, _) = try await URLSession.shared.data(from: imageURL)
+        
+        // Crear una instancia de ImageModel
+        let imageModel = ImageModel(imageData: imageData)
+        return imageModel
     }
-    
 }
 
 enum ImageError: Error {
     case badURL
+    case imageCreationFailed
 }
