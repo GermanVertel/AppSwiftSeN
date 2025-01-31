e715f13da5bc097ba2c58260e00ae4de32cded46
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-01-30 20:05:03 -0500

    se ajusta el contentview para que sea compatible con swift Data

--- AppSwiftSeN/Views/ContentView.swift
+++ AppSwiftSeN/Views/ContentView.swift
@@ -8,35 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var viewModel = DalleViewModel()
+    @State private var viewModel = DalleViewModel()
     @State private var selectedTab = 0
-    
+
     var body: some View {
         NavigationView {
             TabView(selection: $selectedTab) {
-                
                 HomeView(viewModel: viewModel)
                     .tabItem {
                         Label("Generador", systemImage: "photo")
                     }
                     .tag(0)
-                
-                FavoritosView(favoritos: $viewModel.favoritos)
+
+                FavoritosView()
                     .tabItem {
                         Label("Favoritos", systemImage: "heart.fill")
                     }
                     .tag(1)
-                
-               
             }
-            .navigationBarHidden(true) // Oculta el título de la barra de navegación
+            .navigationBarHidden(true)
         }
     }
 }
 
+#Preview {
+  
+    ContentView()
+}
 
 
 #Preview {
     ContentView()
 }
+
 
