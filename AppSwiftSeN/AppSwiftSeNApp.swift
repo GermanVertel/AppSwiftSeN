66a467370d5a281eafa5c2fe0fb0f71d0f55c371
German David Vertel Narvaez <germandavidvertelnarvaez@Germans-Mac-mini.local>
2025-05-07 20:39:03 -0500

    Integración inicial de Firebase y autenticación en iOS: - Agregado Firebase al proyecto iOS. - Añadidas dependencias de Firebase y GoogleSignIn-IO mediante el gestor de paquetes de Xcode. - Incorporado archivo GoogleService-Info.plist al proyecto. - Conectada la app con Firebase. - Habilitados métodos de autenticación: Google y correo/contraseña en Firebase Auth.

--- AppSwiftSeN/AppSwiftSeNApp.swift
+++ AppSwiftSeN/AppSwiftSeNApp.swift
@@ -7,13 +7,25 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct AppSwiftSeNApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .modelContainer(for: ImageModel.self) // Configura el contenedor de SwiftData
+            NavigationView {
+                ContentView()
+                    .modelContainer(for: ImageModel.self) // Configura el contenedor de SwiftData
+            }
+            
         }
     }
 }
